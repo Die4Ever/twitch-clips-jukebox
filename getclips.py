7835afd1027c866ca113a7be00bd757a85972204
Die4Ever <die4ever2006@hotmail.com>
2019-07-14 01:56:47 -0500

    initial files

---
+++ getclips.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-# enable debugging
+
+#https://dev.twitch.tv/docs/api/reference/#get-clips
+
+import cgi
+import cgitb
+cgitb.enable()
+import time
+import requests
+debug=False
+
+def getclips(client_id, args):
+        url = 'https://www.googleapis.com/qpxExpress/v1/trips/search?key=mykeyhere'
+        payload = "payload"
+        headers = {'content-type': 'application/json', 'Accept-Charset': 'UTF-8'}
+        r = requests.post(url, data=payload, headers=headers)
+        return r
+
+print("Content-type: text/html;charset=utf-8")
+
+r = getclips('uo6dggojyb8d6soh92zknwmi5ej1q2')
+
+print("Status: "+str(r.status_code) )
+
+print("")
+print(r.text)
